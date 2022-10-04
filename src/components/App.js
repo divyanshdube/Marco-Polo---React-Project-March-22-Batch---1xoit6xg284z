fb3e49682117ed478b07745721f2abe6545ae3ec
Divyansh Dubey <108198974+divyanshdube@users.noreply.github.com>
2022-10-04 23:01:35 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,23 @@
-import React from 'react'
-import '../styles/App.css';
+import React, { useState } from "react";
+import "../styles/App.css";
 const App = () => {
-  
+  const [showText, setShowText] = useState(false);
+
+  const onClickHandler = () => {
+    if (showText == false) {
+      setShowText(true);
+    } else {
+      setShowText(false);
+    }
+  };
   return (
     <div id="main">
-
+      <h1 id="marco-polo">{showText ? "Polo" : "Marco"}</h1>
+      <button id="marco-polo-toggler" onClick={onClickHandler}>
+        {showText ? "Marco" : "Polo"}
+      </button>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
